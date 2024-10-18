e266ca27c2a8f8b8194ec0e7cc9bd784fa75d752
sahalasyari <114453017+sahalasyari@users.noreply.github.com>
2024-10-18 15:58:34 +0700

    Update equal.py

--- Praktikum Image Enhancement/equal.py
+++ Praktikum Image Enhancement/equal.py
@@ -1,25 +1,32 @@
-import imageio as img
+import imageio.v3 as image
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
-def blend(image1,op1,image2,op2):
-    img1 = image1.astype(np.float32)
-    img2 = image2.astype(np.float32)
-    imgBlend = (img1*op1) + (img2*op2)
-    imgBlend = np.clip (imgBlend,0,255)
-    return imgBlend.astype (np.uint8)
+def equalization(image):
+    hist, bins = np.histogram(image.flatten(), bins = 256, range = [0,256])
+    cdf = hist.cumsum()
+    cdf_normalized = (cdf/cdf.max()) * 255
+    imgEqual = np.interp(image.flatten(), bins [:-1], cdf_normalized)
+    return imgEqual.reshape(image.shape).astype(np.uint8)
 
-img1 = img.imread ("C:\Users\sahal\pcd\image.jpg")
-img2 = img.imread ("C:\Users\sahal\pcd\pngtree-blue-and-purple-neon-star-3d-art-background-with-a-cool-image_3705286.jpg")
+image = image.imread("C:\\Users\\PC S2\\Documents\\low ibu.jpg")
+imageEqual = equalization(image)
 
+hist, bins = np.histogram(image.flatten(), bins=256, range=[0, 256])
+histEqual, bins = np.histogram(imageEqual.flatten(), bins=256, range=[0, 256])
 
-imgBlend =blend (img1,0,3,img2,0.7)
+plt.figure(figsize = (10,10))
+plt.subplot(2,2,1)
+plt.imshow(image)
 
-plt.figure (figsize=(10,10))
-plt.subplot (3,2,1)
-plt.imshow(img1)
-plt.subplot(3,2,3)
-plt.imshow(img2)
-plt.subplot(3,2,5)
-plt.imshow(imgBlend)
+plt.subplot(2,2,2)
+plt.plot(hist)
+
+plt.subplot(2,2,3)
+plt.imshow(imageEqual)
+
+plt.subplot(2,2,4)
+plt.plot(histEqual)
+
+plt.tight_layout()
 plt.show()
